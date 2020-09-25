f35ad02055ecf2289a69628b4d7b850bcc4b8384
Badrinath Reddy Gopugari <badrinath@newzera.com>
2020-09-25 09:55:27 +0530

    optimised dbscan

--- clustering/_dbscan.py
+++ clustering/_dbscan.py
@@ -19,21 +19,20 @@ class DBScan:
                                   < self.eps).sum()) > self.min_points for d in self.data])
 
         # Intial cluster assignment
-        self.cluster_assignment = np.array(
-            [0 for i in range(self.data.shape[0])])
+        self.cluster_assignment = np.zeros(self.data.shape[0])
+
         num_clusters = 1
-        for idx, val in enumerate(core_points):
+        for i, v in enumerate(core_points):
 
-            # If not core point continue
-            if not val:
-                continue
+            # If a core point
+            if v:
 
-            # Assigning cluster to core point
-            if self.cluster_assignment[idx] == 0:
-                self.cluster_assignment[idx] = num_clusters
-                num_clusters += 1
+                # Assigning cluster to core point
+                if self.cluster_assignment[i] == 0:
+                    self.cluster_assignment[i] = num_clusters
+                    num_clusters += 1
 
-            # Assigning cluster to neighbouring points
-            for i, d in enumerate(self.data):
-                if (np.sqrt(((d - self.data[idx])**2).sum()) < self.eps) and (self.cluster_assignment[i] == 0):
-                    self.cluster_assignment[i] = self.cluster_assignment[idx]
+                # Assigning cluster to neighbouring points
+                for j, d in enumerate(self.data):
+                    if (self.cluster_assignment[j] == 0) and (np.sqrt(((d - self.data[i])**2).sum()) < self.eps):
+                        self.cluster_assignment[j] = self.cluster_assignment[i]
